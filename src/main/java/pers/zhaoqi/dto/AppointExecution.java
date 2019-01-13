ac5eec3954ee3be7e71eb4b8a8cbdacf0047a70e
赵奇 <starry-zhao@outlook.com>
2019-01-13 09:57:11 +0800

    完成了数据传输层实体与枚举表示业务结果

--- src/main/java/pers/zhaoqi/dto/AppointExecution.java
+++ src/main/java/pers/zhaoqi/dto/AppointExecution.java
@@ -1,6 +1,7 @@
 package pers.zhaoqi.dto;
 
 import pers.zhaoqi.entity.Appointment;
+import pers.zhaoqi.enums.AppointStateEnum;
 
 public class AppointExecution {
     private long bookId;
@@ -10,4 +11,51 @@ public class AppointExecution {
 
     public AppointExecution() {
     }
+
+    public AppointExecution(long bookId, AppointStateEnum stateEnum) {
+        this.bookId = bookId;
+        this.status = stateEnum.getState();
+        this.stateInfo = stateEnum.getStateInfo();
+    }
+
+    public AppointExecution(long bookId, AppointStateEnum stateEnum, Appointment appointment) {
+        this.bookId = bookId;
+        this.status = stateEnum.getState();
+        this.stateInfo = stateEnum.getStateInfo();
+        this.appointment = appointment;
+    }
+
+    public long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(long bookId) {
+        this.bookId = bookId;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getStateInfo() {
+        return stateInfo;
+    }
+
+    public void setStateInfo(String stateInfo) {
+        this.stateInfo = stateInfo;
+    }
+
+    public Appointment getAppointment() {
+        return appointment;
+    }
+
+    public void setAppointment(Appointment appointment) {
+        this.appointment = appointment;
+    }
+
+
 }
